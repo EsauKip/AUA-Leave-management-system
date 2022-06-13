7b00d85237f5ea8d925bd343ad330ed3f901c494
EsauKip <esau.kiprono@student.moringaschool.com>
2022-06-13 23:23:30 +0300

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HoomeComponent } from './hoome/hoome.component';
+import { ProductsComponent } from './products/products.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { AboutComponent } from './about/about.component';
+import { TvComponent } from './tv/tv.component';
+import { StandingsComponent } from './standings/standings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    HoomeComponent,
+    ProductsComponent,
+    AchievementsComponent,
+    AboutComponent,
+    TvComponent,
+    StandingsComponent
   ],
   imports: [
     BrowserModule,
