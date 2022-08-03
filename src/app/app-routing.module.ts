2c0846aa7955b13b94116dd666652c56c3df50a6
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-03 12:27:50 +0300

    code clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { HoomeComponent } from './hoome/hoome.component';
+import { ProductsComponent } from './products/products.component';
+import { StandingsComponent } from './standings/standings.component';
+import { TvComponent } from './tv/tv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home',pathMatch: 'full' },
+  {path: 'home', component:HoomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path:'achievements',component:AchievementsComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'media',component:TvComponent},
+  {path:'standings',component:StandingsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { navbarData } from './nav-data';
 
 
@@ -14,12 +14,15 @@ interface sideNavToggle{
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
  @Output() onToggleSideNav: EventEmitter<sideNavToggle> = new EventEmitter();
   collapsed = false;
-  screenWidth!: 0;
+  screenWidth!: number;
   navData = navbarData;
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
   toggleCollapse(): void {
     this.collapsed = !this.collapsed;
     this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
@@ -32,3 +35,4 @@ export class SidenavComponent {
   }
 
 }
+ 
